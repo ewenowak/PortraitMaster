b8f5c21db5c105d424389d07574531b6cee39ab2
Ewelina Nowak <ewel.nowak@gmail.com>
2022-07-12 14:20:03 +0200

    Fixed photos.controller.js

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -10,47 +10,30 @@ exports.add = async (req, res) => {
     const { title, author, email } = req.fields;
     const file = req.files.file;
 
-    const textPattern = new RegExp(/(<\s*(strong|em)*>(([A-z]|\s)*)<\s*\/\s*(strong|em)>)|(([A-z]|\s|\.)*)/, 'g');
-    const titleMatched = title.match(textPattern).join('');
-    const authorMatched = author.match(textPattern).join('');
-
-    const emailPattern = newRegExp(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/, 'g');
-    const emailMatched = email.match(emailPattern).join('');
-
-    if(title && author && email && file) { // if fields are not empty...
-       
-      if(title.length > 25) {
-        throw new Error('Tittle is too long, use max 25 characters');
-      } else if (author.length > 50) {
-        throw new Error('Author is too long, use max 50 characters')
-      }
-
-      if (titleMatched.length < title.length) {
-        throw new Error('Used in title invalid characters..');
-      } else if (authorMatched.length < author.length) {
-        throw new Error('Used in author name invalid characters...');
-      } else if (emailMatched.length < email.length) {
-        throw new Error('Used in email invalid characters...');
-      }
-
+    if(title.length <= 25 && author.length <= 50 && email && file) {
       const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
-      const fileExtension = file.split('.').slice(-1)[0]
-      
-      if(fileExtension !== 'jpg' || fileExtension !== 'png' || fileExtension !== 'gif') {
-        throw new Error('File extension is wrong');
+      const fileExtension = file.path.split('.').slice(-1)[0]
+
+      const textPattern = new RegExp(/(<\s*(strong|em)*>(([A-z]|\s)*)<\s*\/\s*(strong|em)>)|(([A-z]|\s|\.)*)/, 'g');
+      const titleMatched = title.match(textPattern).join('');
+      const authorMatched = author.match(textPattern).join('');
+
+      const emailPattern = new RegExp(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/, 'g');
+      const emailMatched = email.match(emailPattern).join('');
+
+      if((fileExtension === 'jpg' || fileExtension === 'png' || fileExtension === 'gif') && title === titleMatched && author === authorMatched && email === emailMatched) {
+        const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
+        await newPhoto.save(); // ...save new photo in DB
+        res.json(newPhoto);
+      } else {
+          throw new Error('Wrong input!');
       }
-      const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
-      await newPhoto.save(); // ...save new photo in DB
-      res.json(newPhoto);
-
     } else {
-      throw new Error('Wrong input!');
+        throw new Error('Wrong input!');
     }
-
   } catch(err) {
-    res.status(500).json(err);
+      res.status(500).json(err);
   }
-
 };
 
 /****** LOAD ALL PHOTOS ********/
